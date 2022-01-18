f72743926a6832cde0194e1bc8fd05012eb028d4
forgetdark <forgetdark@gmail.com>
2022-01-18 10:58:15 +0800

    Update Melonbooks.js

--- Melonbooks.js
+++ Melonbooks.js
@@ -6,4 +6,11 @@ javascript:(function(){
       thumbs_list.push($(thumb).find('a').attr('href'));
   });
   var description = $('#description').find('p').html();
+  var stripe = document.querySelectorAll('.odd');
+  var object = [];
+  stripe.forEach(function(item, index) {
+      var th = item.children[0].innerText;
+      var td = item.children[1].innerText;
+      object[th] = td;
+  });
 });
