a2c4a0ef1842f67d8efd673e082c0d8b1a034372
forgetdark <forgetdark@gmail.com>
2022-01-19 11:28:25 +0800

    Update Pixiv.js

--- Pixiv.js
+++ Pixiv.js
@@ -1,19 +1,15 @@
 javascript:(function(){
-try {
-  var copy_msg = location.href;
-  navigator.clipboard.writeText(copy_msg)
-  .then(() => {
-    console.log("Text copied to clipboard...");
-  })
-  .catch(err => {
-    console.log('Something went wrong', err);
-  });
-} catch (error) {
-  console.log('Oops!, unable to copy');
-}
-document.querySelector('.lfwBiP').addEventListener('click', function(event){
+  var el_list = {
+    'cover': {
+      'link': '.sc-1u8nu73-18',
+      'img': '.sc-1u8nu73-19'
+    },
+    'title': '.sc-1u8nu73-3',
+    'description': '.sc-eyxzap-1',
+    'content': '.sc-dIvrsQ'
+  };
   try {
-    var copy_msg = document.querySelector('.lfwBiP').innerText;
+    var copy_msg = location.href;
     navigator.clipboard.writeText(copy_msg)
     .then(() => {
       console.log("Text copied to clipboard...");
@@ -24,33 +20,39 @@ document.querySelector('.lfwBiP').addEventListener('click', function(event){
   } catch (error) {
     console.log('Oops!, unable to copy');
   }
-});
-document.querySelector('.llrjLt').addEventListener('click', function(event){
-  try {
-    var copy_msg = document.querySelector('.llrjLt').innerText;
-    navigator.clipboard.writeText(copy_msg)
-    .then(() => {
-      console.log("Text copied to clipboard...");
-    })
-    .catch(err => {
-      console.log('Something went wrong', err);
-    });
-  } catch (error) {
-    console.log('Oops!, unable to copy');
+  document.querySelector(el_list.cover.link).setAttribute('href','javascript:void(0);');
+  document.querySelector(el_list.cover.link).removeAttribute('target');
+  for (const [key, el] of Object.entries(el_list)) {
+    if (key == 'cover') {
+      document.querySelector(el.link).addEventListener('click', function(event){
+        try {
+          var copy_msg = document.querySelector(el.img).getAttribute('src');
+          navigator.clipboard.writeText(copy_msg)
+          .then(() => {
+            console.log("Text copied to clipboard...");
+          })
+          .catch(err => {
+            console.log('Something went wrong', err);
+          });
+        } catch (error) {
+          console.log('Oops!, unable to copy');
+        }
+      });
+    } else {
+      document.querySelector(el).addEventListener('click', function(event){
+        try {
+          var copy_msg = document.querySelector(el).innerText;
+          navigator.clipboard.writeText(copy_msg)
+          .then(() => {
+            console.log("Text copied to clipboard...");
+          })
+          .catch(err => {
+            console.log('Something went wrong', err);
+          });
+        } catch (error) {
+          console.log('Oops!, unable to copy');
+        }
+      });
+    }
   }
-});
-document.querySelector('.leiCDy').addEventListener('click', function(event){
-  try {
-    var copy_msg = document.querySelector('.leiCDy').innerText;
-    navigator.clipboard.writeText(copy_msg)
-    .then(() => {
-      console.log("Text copied to clipboard...");
-    })
-    .catch(err => {
-      console.log('Something went wrong', err);
-    });
-  } catch (error) {
-    console.log('Oops!, unable to copy');
-  }
-});
 })();
