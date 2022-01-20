fa70c84165fdf2db4b88cf01f2297e89ebfdac20
forgetdark <forgetdark@gmail.com>
2022-01-20 14:32:18 +0800

    Update Pixiv.js

--- Pixiv.js
+++ Pixiv.js
@@ -1,4 +1,40 @@
 javascript:(function(){
+  var style = document.createElement('style');
+  style.innerHTML = `.tooltiptext {
+    visibility: hidden;
+    width: 120px;
+    background-color: #555;
+    color: #fff;
+    text-align: center;
+    padding: 5px 0;
+    border-radius: 6px;
+    position: fixed;
+    z-index: 1;
+    bottom: 5%;
+    left: 5%;
+    margin-left: -60px;
+    opacity: 0;
+    transition: opacity 0.3s;
+  }
+  .tooltiptext::after {
+    content: "";
+    position: absolute;
+    top: 100%;
+    left: 50%;
+    margin-left: -5px;
+    border-width: 5px;
+    border-style: solid;
+    border-color: #555 transparent transparent transparent;
+  }
+  .tooltiptext_hover {
+    visibility: visible;
+    opacity: 1;
+  }`;
+  document.body.appendChild(style);
+  var tooltiptext = document.createElement('div'); 
+  tooltiptext.innerHTML = '複製完成';
+  tooltiptext.classList.add('tooltiptext');
+  document.body.appendChild(tooltiptext);
   var el_list = {
     'cover': {
       'link': '.sc-1u8nu73-18',
@@ -30,6 +66,10 @@ javascript:(function(){
           navigator.clipboard.writeText(copy_msg)
           .then(() => {
             console.log("Text copied to clipboard...");
+            document.querySelector('.tooltiptext').classList.add('tooltiptext_hover');
+            setTimeout(function () {
+              document.querySelector('.tooltiptext').classList.remove('tooltiptext_hover');
+            }, 1000);
           })
           .catch(err => {
             console.log('Something went wrong', err);
@@ -45,6 +85,10 @@ javascript:(function(){
           navigator.clipboard.writeText(copy_msg)
           .then(() => {
             console.log("Text copied to clipboard...");
+            document.querySelector('.tooltiptext').classList.add('tooltiptext_hover');
+            setTimeout(function () {
+              document.querySelector('.tooltiptext').classList.remove('tooltiptext_hover');
+            }, 1000);
           })
           .catch(err => {
             console.log('Something went wrong', err);
