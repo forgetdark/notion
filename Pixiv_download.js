cf3050e4614e9fe3f6181bbe70cf88eb628f1007
forgetdark <forgetdark@gmail.com>
2022-02-08 13:16:17 +0800

    Update and rename Pixiv_copy.js to Pixiv_download.js

---
+++ Pixiv_download.js
@@ -0,0 +1,22 @@
+javascript:(function(){
+  var el_list = {
+    'title': '.sc-1u8nu73-3',
+    'content': '.sc-dIvrsQ'
+  };
+  try {
+    var text = document.querySelector(el_list.content).innerText;
+    var filename = document.querySelector(el_list.title).innerText;
+
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+    document.body.removeChild(element);
+  } catch (error) {
+    console.log('Oops!, unable to download');
+  }
+})();
