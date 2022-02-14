8f24809115102bd4b44e48d30f8c86434ff431eb
forgetdark <forgetdark@gmail.com>
2022-02-14 18:45:09 +0800

    Update Pixiv.js

--- Pixiv.js
+++ Pixiv.js
@@ -1,41 +1,47 @@
 javascript:(function(){
-  var style = document.createElement('style');
-  style.innerHTML = `.tooltiptext {
-    visibility: hidden;
-    width: 100px;
-    background-color: #555;
-    color: #fff;
-    text-align: center;
-    padding: 5px 0;
-    border-radius: 6px;
-    position: fixed;
-    z-index: 1;
-    bottom: 3%;
-    left: 5%;
-    margin-left: -60px;
-    opacity: 0;
-    transition: opacity 0.3s;
-    font-size: 13px;
-  }
-  .tooltiptext::after {
-    content: "";
-    position: absolute;
-    top: 100%;
-    left: 50%;
-    margin-left: -5px;
-    border-width: 5px;
-    border-style: solid;
-    border-color: #555 transparent transparent transparent;
-  }
-  .tooltiptext_hover {
-    visibility: visible;
-    opacity: 1;
-  }`;
-  document.body.appendChild(style);
-  var tooltiptext = document.createElement('div'); 
-  tooltiptext.innerHTML = '複製完成';
-  tooltiptext.classList.add('tooltiptext');
-  document.body.appendChild(tooltiptext);
+  var $tooptip = (function() {
+    var tooptipStyle = document.createElement('style');
+    tooptipStyle.id = 'tooltip-style';
+    tooptipStyle.innerHTML = `.tooltip-model {
+      visibility: hidden;
+      width: 100px;
+      background-color: #555;
+      color: #fff;
+      text-align: center;
+      padding: 5px 0;
+      border-radius: 6px;
+      position: fixed;
+      z-index: 99999999;
+      left: 1%;
+      bottom: 3%;
+      transition: opacity 0.3s;
+      font-size: 13px;
+    }
+    .tooltip-model:after {
+      content: "";
+      position: absolute;
+      top: 100%;
+      left: 50%;
+      margin-left: -5px;
+      border-width: 5px;
+      border-style: solid;
+      border-color: #555 transparent transparent transparent;
+    }`;
+    var tooltipModel = document.createElement('div');
+    tooltipModel.innerHTML = '複製成功';
+    tooltipModel.classList.add('tooltip-model');
+    return {
+      show: function () {
+        document.body.appendChild(tooptipStyle);
+        document.body.appendChild(tooltipModel);
+        document.querySelector('.tooltip-model').style.visibility = 'visible';
+      },
+      hide: function () {
+        document.getElementById('tooltip-style').remove();
+        document.querySelector('.tooltip-model').remove();
+      }
+    };
+  })();
   var elementList = {
     'cover': {
       'link': '.sc-1u8nu73-18',
@@ -50,9 +56,9 @@ javascript:(function(){
       navigator.clipboard.writeText(copyMsg)
       .then(() => {
         console.log("Text copied to clipboard...");
-        document.querySelector('.tooltiptext').classList.add('tooltiptext_hover');
+        $tooptip.show();
         setTimeout(function () {
-          document.querySelector('.tooltiptext').classList.remove('tooltiptext_hover');
+          $tooptip.hide();
         }, 1000);
       })
       .catch(err => {
