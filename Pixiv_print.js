98c61ae39843b022b85f9af8a57d2e8ca8459baf
forgetdark <forgetdark@gmail.com>
2022-03-23 05:14:24 +0800

    Update Pixiv_print.js

--- Pixiv_print.js
+++ Pixiv_print.js
@@ -144,7 +144,6 @@ javascript:(function(){
   var $printTxt = function (text) {
     try {
       $loader.hide();
-      $copyTextOfElement($getFileName());
       var newWin = window.open('', 'print window');
       newWin.document.open();
       newWin.document.write(`<html>
@@ -167,6 +166,7 @@ javascript:(function(){
     };
   };
   $loader.show();
+  $copyTextOfElement($getFileName());
   if (document.querySelectorAll('.kYtoqc').length > 0) {
     var textList = [];
     var $saveText = function() {
