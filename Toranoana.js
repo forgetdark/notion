670270c4570b7b710b321034c999feafb5828d2a
forgetdark <forgetdark@gmail.com>
2022-03-31 11:13:42 +0800

    Update Toranoana.js

--- Toranoana.js
+++ Toranoana.js
@@ -73,6 +73,41 @@ javascript:(function(){
       }
     };
   })();
+
+  var $copyTextOfElement = function (copyText) {
+    function copyEl(text) {
+      var el = document.createElement("textarea");
+      el.value = text;
+      document.body.appendChild(el);
+      el.focus();
+      el.select();
+      var copyStatus = document.execCommand('copy');
+      var msg = copyStatus ? 'copied' : 'failed';
+      var transMsg = copyStatus ? '複製成功' : '複製失敗';
+      document.body.removeChild(el);
+      console.log('Text ' + msg + ' to clipboard...');
+      $tooptip.show(transMsg);
+      setTimeout(function () {
+        $tooptip.hide();
+      }, 1000);
+    };
+    if (!navigator.clipboard) {
+      copyEl(copyText);
+    } else {
+      let resolve = () => { 
+        console.log('Text copied to clipboard...');
+        $tooptip.show('複製成功');
+        setTimeout(function () {
+          $tooptip.hide();
+        }, 1000);
+      };
+      let reject = (err) => {
+        copyEl(copyText);
+      };
+      navigator.clipboard.writeText(copyText).then(resolve, reject);
+    }
+  };
+
   if ($('.btn-copy').length == 0) {
     var style = document.createElement('style');
     style.innerHTML = `.btn-copy {
@@ -86,20 +121,26 @@ javascript:(function(){
       font-size: 13px;
     }`;
     document.body.appendChild(style);
+
     var info = '.product-detail-infosub-btn-area';
     var url = location.href;
     $(info).append('<button type="button" class="btn-copy" data-type="url" data-content="'+url+'">連結</button>');
+
     var cover = $('#preview img').attr('src');
     $(info).append('<button type="button" class="btn-copy" data-type="cover" data-content="'+cover+'">封面</button>');
+
     var thumbs = document.querySelectorAll('.product-detail-image-thumb-item');
     thumbs.forEach(function (thumb, index) {
       var img = $(thumb).data('src');
       $(info).append('<button type="button" class="btn-copy" data-type="thumbs" data-content="'+img+'">縮圖'+(index+1)+'</button>');
+
     });
     var description = $('.product-detail-comment-item').find('p').text();
     $(info).append('<button type="button" class="btn-copy" data-type="description" data-content="'+description+'">描述</button>');
+
     var title = $('.product-detail-desc-title').find('span').text();
     $(info).append('<button type="button" class="btn-copy" data-type="stripe" data-content="'+title+'">タイトル</button>');
+
     var stripe = document.querySelectorAll('.product-detail-spec-table tr');
     var object = [];
     stripe.forEach(function(item, index) {
@@ -108,29 +149,10 @@ javascript:(function(){
       object[th] = td;
       $(info).append('<button type="button" class="btn-copy" data-type="stripe" data-content="'+td+'">'+th+'</button>');
     });
+
     $('.btn-copy').on('click', function (event) {
-      var type = $(this).data('type');
-      var copyMsg = '';
-      switch(type) {
-        default:
-          copyMsg = $(this).data('content');
-          break;
-      };
-      try {
-        navigator.clipboard.writeText(copyMsg)
-        .then(() => {
-          console.log("Text copied to clipboard...");
-          $tooptip.show('複製成功');
-          setTimeout(function () {
-            $tooptip.hide();
-          }, 1000);
-        })
-        .catch(err => {
-          console.log('Something went wrong', err);
-        });
-      } catch (error) {
-        console.log('Oops!, unable to copy');
-      }
+      var content = $(this).data('content');
+      $copyTextOfElement(content);
     });
   }
 })();
