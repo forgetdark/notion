6af1e31be69a5a1a4e9698393152e4af35f56b66
forgetdark <forgetdark@gmail.com>
2022-03-31 10:36:28 +0800

    Update Books.js

--- Books.js
+++ Books.js
@@ -106,99 +106,40 @@ javascript:(function(){
       navigator.clipboard.writeText(copyText).then(resolve, reject);
     }
   };
-  if (location.href.indexOf('series') < 0) {
-    var elementList = {
-      'cover': {
-        'link': '.sc-1u8nu73-18',
-        'img': '.sc-1u8nu73-19'
-      },
-      'series': '.sc-1u8nu73-15',
-      'title': '.sc-1u8nu73-3',
-      'chapter': ['.sc-jrsJWt', '.'+document.querySelector('#gtm-novel-work-scroll-finish-reading').previousElementSibling.previousElementSibling.classList[0]],
-      'description': '.sc-eyxzap-1',
-      'content': ['.sc-dIvrsQ', '.'+document.querySelector('#gtm-novel-work-scroll-finish-reading').previousElementSibling.classList[0]],
-      'pages': '.sc-xhhh7v-1'
-    };
-    for (const [key, el] of Object.entries(elementList)) {
-      if (key == 'cover') {
-        document.querySelector(elementList.cover.link).setAttribute('href','javascript:void(0);');
-        document.querySelector(elementList.cover.link).removeAttribute('target');
-        document.querySelector(el.link).addEventListener('click', function(event) {
-          $copyTextOfElement(document.querySelector(el.img).getAttribute('src'));
-        });
-      } else if (key == 'chapter' || key == 'content') {
-        el.forEach (function (e) {
-          var contents = document.querySelectorAll(e);
-          if (contents.length > 0) {
-            [].forEach.call(contents, function(element) {
-              element.addEventListener('click', function(event) {
-                $copyTextOfElement(this.innerText);
-              });
-            });
-          }
-        });
-      } else if (key == 'pages') {
-        var pages = document.querySelectorAll(el);
-        if (pages.length > 0) {
-          [].forEach.call(pages, function(page) {
-            page.addEventListener('click', function(event) {
-              setTimeout(function () {
-                for (const [index, e] of Object.entries(elementList.chapter)) {
-                  var chapters = document.querySelectorAll(e);
-                  if (chapters.length > 0) {
-                    [].forEach.call(chapters, function(element) {
-                      element.addEventListener('click', function(event) {
-                        $copyTextOfElement(this.innerText);
-                      });
-                    });
-                  }
-                }
-                for (const [index, e] of Object.entries(elementList.content)) {
-                  var contents = document.querySelectorAll(e);
-                  if (contents.length > 0) {
-                    [].forEach.call(contents, function(element) {
-                      element.addEventListener('click', function(event) {
-                        $copyTextOfElement(this.innerText);
-                      });
-                    });
-                  }
-                }
-              }, 100);
-            });
-          });
-          $tooptip.show('有分頁');
-          setTimeout(function () {
-            $tooptip.hide();
-          }, 1000);
-        } else {
-          $tooptip.show('無分頁');
-          setTimeout(function () {
-            $tooptip.hide();
-          }, 1000);
-        }
-      } else {
-        if (document.querySelectorAll(el).length > 0) {
-          document.querySelector(el).addEventListener('click', function(event) {
+  var elementList = {
+    'cover': '.cover',
+    'title': 'h1',
+    'author': '#trace_btn1',
+    'publisher': '#trace_btn2',
+    'content': '.content',
+    'more': '.more'
+  };
+  for (const [key, el] of Object.entries(elementList)) {
+    if (key == 'cover') {
+      document.querySelector(el).addEventListener('click', function(event) {
+        $copyTextOfElement(this.getAttribute('src'));
+      });
+    } else if (key == 'author' || key == 'publisher') {
+      if (document.querySelectorAll(el).length > 0) {
+        $copyTextOfElement(document.querySelector(el).previousElementSibling.innerText);
+      }
+    } else if (key == 'content') {
+      var contents = document.querySelectorAll(e);
+      if (contents.length > 0) {
+        [].forEach.call(contents, function(element) {
+          element.addEventListener('click', function(event) {
             $copyTextOfElement(this.innerText);
           });
-        }
-      }
-    }
-  } else {
-    var elementList = {
-      'series': '.sc-vk2fvc-2',
-      'description': '.sc-eyxzap-1'
-    };
-    for (const [key, el] of Object.entries(elementList)) {
-      if (document.querySelectorAll(el).length > 0) {
-        document.querySelector(el).addEventListener('click', function(event) {
-          $copyTextOfElement(this.innerText);
         });
       }
+    } else if (key == 'more') {
+      if (document.querySelectorAll(el).length > 0) {
+        document.querySelector(el).click();
+      }
+    } else {
+      if (document.querySelectorAll(el).length > 0) {
+        $copyTextOfElement(document.querySelector(el).innerText);
+      }
     }
-    $tooptip.show('準備就緒');
-    setTimeout(function () {
-      $tooptip.hide();
-    }, 1000);
   }
 })();
