9599a44365cb2116af8107f7c4ff018b9deb3509
forgetdark <forgetdark@gmail.com>
2023-07-12 14:59:35 +0800

    Update Privatter.js

--- Privatter.js
+++ Privatter.js
@@ -174,8 +174,8 @@ javascript:(function(){
       content+= `<div><div class="panel-copy">`+url+`</div>`+
       `<h1 class="panel-copy">`+document.querySelector('.lead').innerText+`</h1>`+
       `<div class="panel-copy" style="float: left;">`+document.querySelector('[name="userprof"]').nextElementSibling.innerHTML+`</div>`+
-      `<div class="panel-copy" style="float: right;">`+document.querySelector('.lead').nextElementSibling.nextElementSibling.innerHTML+`</div>`+
-      `<hr style="clear: both;"><div class="panel-copy">`+document.querySelector('.fa-clock').parentElement.parentElement.nextElementSibling.innerHTML+`</div></div>`;
+      `<div class="panel-copy" style="float: right;">`+document.querySelector('.lead').nextElementSibling.nextElementSibling.innerHTML+`</div>`;
+      content+= `<hr style="clear: both;"><div class="panel-copy">`+document.querySelector('.fa-clock').parentElement.parentElement.nextElementSibling.innerHTML+`</div></div>`;
       content+= '<hr>';
     }
     return content;
