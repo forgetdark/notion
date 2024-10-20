1a0b44dbc6a9f88c330c3f3324d4f8483683bdc9
Wen <forgetdark@gmail.com>
2024-10-20 15:59:04 +0800

    Create youtube.js

---
+++ youtube.js
@@ -0,0 +1,22 @@
+javascript:(() => {
+  const createYoutubeStyle = () => {
+    const style = document.createElement('style');
+    style.id = 'youtube-style';
+    style.innerHTML = `
+      ytd-rich-item-renderer[rendered-from-rich-grid][is-in-first-column] {
+        margin: 5px !important;
+      }
+      ytd-rich-item-renderer {
+        margin: 5px !important;
+      }`;
+    return style;
+  };
+  
+  const existingStyle = document.getElementById('youtube-style');
+  if (!existingStyle) {
+    const YTstyle = createYoutubeStyle();
+    document.body.appendChild(YTstyle);
+  } else {
+    existingStyle.remove();
+  }
+})();
